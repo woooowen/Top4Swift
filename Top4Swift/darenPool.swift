aadcac34ea3d7463b0efb6a65fc0e40bfac960c8
woooowen <woooowen@gmail.com>
2014-11-20 20:06:50 +0800

    完成基本布局

---
+++ Top4Swift/darenPool.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Top4Swift
+//
+//  Created by james on 14-11-18.
+//  Copyright (c) 2014年 woowen. All rights reserved.
+//
+
+import UIKit
+
+class darenPool: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
