fc7099e97b8814e171a0ed048fb721ce5ea35a49
woooowen <woooowen@gmail.com>
2014-11-20 19:28:29 +0800

    init

---
+++ Top4Swift/HttpController.swift
@@ -0,0 +1,20 @@
+
+import Foundation
+
+protocol HttpProtocol{
+    func didRecieveResult(result: NSDictionary)
+}
+class HttpController: NSObject{
+    
+    var delegate: HttpProtocol?
+    
+    func get(url: String){
+        var nsUrl: NSURL = NSURL(string: url)!
+        var request: NSURLRequest = NSURLRequest(URL: nsUrl)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!)->Void in
+            var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+            self.delegate?.didRecieveResult(jsonResult)
+            
+        })
+    }
+}
---
+++ Top4Swift/LoginController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Top4Swift
+//
+//  Created by james on 14-11-18.
+//  Copyright (c) 2014年 woowen. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    @IBOutlet weak var Uname: UITextField!
+    @IBOutlet weak var Pwd: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //点击登录
+    @IBAction func LoginBtnClick(sender: AnyObject) {
+        let url = "http://top.mogujie.com/app_top_v142_login/mobilelogin?_swidth=720&_channel=NAOtop&_atype=android&_sdklevel=18&_network=2&_fs=NAOtop142&_did=99000537220553&_aver=142&_source=NAOtop142"
+        self.performSegueWithIdentifier("login", sender: self)        
+    }
+    
+    //点击其他部位隐藏虚拟键盘
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        Uname.resignFirstResponder()
+        Pwd.resignFirstResponder()
+    }
+    
+}
+
