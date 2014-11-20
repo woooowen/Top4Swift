ad4002faff39cbeb4e1cdd2adfa0a62533ba76de
woooowen <woooowen@gmail.com>
2014-11-20 20:10:36 +0800

    add refresh plugins

--- normalPool.swift
+++ normalPool.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class normalPool: UIViewController {
     
+    var url = "http://top.mogujie.com/top/zadmin/app/index?sign=Mx3KdFcp1pGbaU4PLk82p9sAON6%2FXfJwJjiKf%2FjNMD8J3YyXyjPQS%2FUUQmMMjduXNoZXMsS6cXMF66wmRMs%2Bsw%3D%3D"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
