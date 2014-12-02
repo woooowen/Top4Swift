662b18ce167a3526af8abb429031ef940589ea27
woooowen <woooowen@gmail.com>
2014-12-02 19:25:36 +0800

    add btn animate for detailView

--- Top4Swift/detailViewController.swift
+++ Top4Swift/detailViewController.swift
@@ -13,13 +13,18 @@ class detailViewController: UIViewController,UIWebViewDelegate {
     
     @IBOutlet weak var loadImg: UIActivityIndicatorView!
     
+    @IBOutlet weak var accessBtn: UIButton!
+    
+    @IBOutlet weak var checkBtn: UIButton!
+    
+    @IBOutlet weak var offShelfBtn: UIButton!
+    
     var timeLineUrl: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         webView.delegate = self
         timeLineUrl = "http://top.mogujie.com/top/share/note?tid=" + timeLineUrl
-        println(self.timeLineUrl)
         var url = NSURL(string: self.timeLineUrl)
         var request = NSURLRequest(URL: url!)
         webView.loadRequest(request)
@@ -39,6 +44,39 @@ class detailViewController: UIViewController,UIWebViewDelegate {
     func webViewDidFinishLoad(webView: UIWebView){
         loadImg.stopAnimating()
         loadImg.removeFromSuperview()
+    }
+    //看过按钮点击事件
+    @IBAction func accessBtnClick(sender: AnyObject) {
+        var btnName = accessBtn.titleForState(UIControlState.Normal)
+        if(btnName == "通过"){
+            accessBtn.setTitle("已通过", forState: UIControlState.Normal)
+            accessBtn.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+        }else{
+            accessBtn.setTitle("通过", forState: UIControlState.Normal)
+            accessBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        }
+    }
+    //看过按钮点击事件
+    @IBAction func checkBtnClick(sender: AnyObject) {
+        var btnName = checkBtn.titleForState(UIControlState.Normal)
+        if(btnName == "看过"){
+            checkBtn.setTitle("已看过", forState: UIControlState.Normal)
+            checkBtn.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+        }else{
+            checkBtn.setTitle("看过", forState: UIControlState.Normal)
+            checkBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        }
+    }
+    //下架按钮点击事件
+    @IBAction func offShelfBtnClick(sender: AnyObject) {
+        var btnName = offShelfBtn.titleForState(UIControlState.Normal)
+        if(btnName == "下架"){
+            offShelfBtn.setTitle("已下架", forState: UIControlState.Normal)
+            offShelfBtn.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+        }else{
+            offShelfBtn.setTitle("下架", forState: UIControlState.Normal)
+            offShelfBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        }
     }
     
 }
