d9cbd569a2f26b23da47fffd1a0d66054913f533
woooowen <woooowen@gmail.com>
2014-12-16 11:51:42 +0800

    修复分类不跳转的问题

--- Top4Swift/LoginController.swift
+++ Top4Swift/LoginController.swift
@@ -31,7 +31,7 @@ class LoginController: UIViewController,HttpProtocol{
         let url = "http://top.mogujie.com/app_top_v151_login/mobilelogin?_swidth=720&_channel=NAOtopGrey&_atype=android&_sdklevel=18&_network=2&_fs=NAOtopGrey151&_did=99000537220553&_aver=150&_source=NAOtopGrey151"
         let params = ["mobile" : self.Uname.text , "pwd" : self.Pwd.text]
 
-        self.performSegueWithIdentifier("login", sender: self)
+//        self.performSegueWithIdentifier("login", sender: self)
 
         eHttp.post(url, params: params, callback: {(data: NSDictionary) -> Void in
             var code = false
